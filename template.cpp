f41857e8e3c98a3ba7e444cfdb37687092a2fd5b
Daniel Bustamante <vtdanielf@gmail.com>
2026-01-19 22:35:59 -0500

    Template para ejercicios

---
+++ template.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//macros
+#define endl '\n'
+//alias
+using ll = long long;
+//int 32 bits -2^31 a 2^31-1
+//long long 64 bits -2^63 a 2^63-1
+
+
+void solve(){
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t=1;
+    //cin>>t;
+
+    while (t--)
+    {
+     solve();   
+    }
+}
